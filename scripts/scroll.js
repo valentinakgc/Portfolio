97e1a84b2d17d515039f6874b8de185cebd74717
Valentina Kroth Gil Cardoso <valentinakgc@gmail.com>
2023-10-29 20:01:48 -0300

    fixed nav

---
+++ scripts/scroll.js
@@ -0,0 +1,12 @@
+const solto = "my-4 absolute w-screen"
+const fixo = "bg-white mb-4 pt-4 inset-x-0 top-0 fixed w-screen z-50"
+document.addEventListener("scroll", (event) => {
+    let barra = document.getElementById("barra")
+    if (window.scrollY >= 200) { 
+        // verifica se ja ta fixo
+        barra.classList.value = fixo
+     }
+     else {
+        barra.classList.value = solto
+     }
+})
